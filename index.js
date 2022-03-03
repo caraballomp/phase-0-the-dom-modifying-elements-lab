3629d6228f7ac9f64971849d5dd6440eadd1958f
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-02 19:14:24 -0800

    commit tree cbd18a11ac0adb6b4ce0810b088f7990bc1952c8

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+// element.append(ul);
+
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+
+
 const newHeader=document.querySelector('h1#victory');
 newHeader.innerHTML="Michelle is the champion"
